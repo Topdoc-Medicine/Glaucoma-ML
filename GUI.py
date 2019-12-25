87347bdb6a585bb79807a05b31a26b5d592e53a6
rvignav <rvignav@gmail.com>
2019-12-24 20:53:06 -0800

    polished GUI.py code

--- GUI.py
+++ GUI.py
@@ -9,13 +9,10 @@ import pandas
 import h5py
 import glob
 
-# read HDF5 file
 h5file =  "dataweightsvha.h5"
 
-# this command allows you to close the file automatically after loading
 with h5py.File(h5file,'r') as fid:
      model = load_model(fid)
-#model = load_model('f1.h5')
 
 def get_filenames():
     global path
@@ -43,7 +40,6 @@ def autoroi(img):
 def prediction():
     list_of_files = glob.glob('data/test/*')
     latest_file = max(list_of_files, key=os.path.getctime)
-    print(latest_file)
     img = cv2.imread(latest_file)
     img = autoroi(img)
     img = cv2.resize(img, (256, 256))
@@ -57,105 +53,6 @@ def prediction():
 
 Class = prediction()
 if (Class == 0):
-    print("Prediction: You have been diagnosed with Glaucoma")
+    print("Unfortunately, you have been diagnosed with glaucoma.")
 else:
-    print("Prediction: Congratulations! You are Healthy")
-"""
-def run():
-
-    Class = prediction()
-    if (Class == 0):
-        messagebox.showinfo('Prediction', 'You have been diagnosed with Glaucoma')
-    else:
-        messagebox.showinfo('Prediction', 'Congratulations! You are Healthy')
-
-
-def run_all():
-
-    x = os.listdir(path)
-    y = []
-    affected = 0
-
-    for i in x:
-        img = cv2.imread('test/%s' % (i))
-        img = autoroi(img)
-        img = cv2.resize(img, (256, 256))
-        img = np.reshape(img, [1, 256, 256, 3])
-
-        prob = model.predict(img)
-        Class = prob.argmax(axis=-1)
-        y.append(Class[0])
-        if Class == 1:
-            affected += 1
-
-    df = pandas.DataFrame(data=y, index=x, columns=["output"])
-    df.to_csv('output.csv', sep=',')
-
-
-def ROI():
-    img = cv2.imread('test/%s' % (spath))
-    roi = autoroi(img)
-    cv2.imshow("Region of Interest", roi)
-
-
-def preview():
-    img = cv2.imread('test/%s' % (spath))
-    cv2.imshow('Image', img)
-
-
-def graph():
-
-    total = len(os.listdir(path))
-    affected = pandas.read_csv('output.csv')
-    affected = affected['output'].sum()
-
-    healthy = total - affected
-
-    piey = ["Glaucomatous", "Healthy"]
-    piex = [affected, healthy]
-
-    plt.axis("equal")
-    plt.pie(piex, labels=piey, radius=1.5, autopct='%0.1f%%', explode=[0.2, 0])
-    plt.show()
-
-# Frontend GUI
-
-
-window = Tk()
-window.title("Glaucoma Detection")
-window.geometry('1000x550')
-window.configure(background='grey')
-
-l1 = Label(window, text="Test Image", font=("Arial", 20), padx=10, bg='grey')
-l1.grid(row=0, column=0)
-
-b1 = Button(window, text='Run', font=("Arial", 20), command=run)
-b1.grid(row=1, column=3)
-
-b2 = Button(window, text='Preview', font=("Arial", 20), command=preview)
-b2.grid(row=1, column=2, rowspan=2, padx=10)
-
-b2 = Button(window, text='ROI', font=("Arial", 20), command=ROI)
-b2.grid(row=2, column=2, rowspan=3, padx=10)
-
-b3 = Button(window, text='Run all', font=("Arial", 20), command=run_all)
-b3.grid(row=2, column=3)
-
-b4 = Button(window, text='Graph', font=("Arial", 20), command=graph)
-b4.grid(row=3, column=3)
-
-t1 = Listbox(window, height=20, width=60, selectmode=SINGLE, font=("Arial", 15), justify=CENTER)
-t1.grid(row=1, column=0, rowspan=3, padx=10)
-for filename in get_filenames():
-    t1.insert(END, filename)
-t1.bind('<<ListboxSelect>>', curselect)
-
-sb1 = Scrollbar(window)
-sb1.grid(row=1, column=1, rowspan=4)
-
-t1.configure(yscrollcommand=sb1.set)
-sb1.configure(command=t1.yview)
-
-
-window.mainloop()
-"""
+    print("Congratulations! You are healthy!")

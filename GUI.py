c46e7aca13fdef160bf0c073281f749045b71761
ayaanzhaque <ayaanzhaque@gmail.com>
2020-03-22 16:34:57 -0700

    Tested VGG Model, works aorund 60-75% of the time, outputs decimals

--- GUI.py
+++ GUI.py
@@ -11,7 +11,7 @@ import pandas
 import h5py
 import glob
 
-h5file =  "CNN3WeightsS.h5"
+h5file =  "CNN4Weights.h5"
 
 with h5py.File(h5file,'r') as fid:
      model = tf.keras.models.load_model(fid)
@@ -27,31 +27,31 @@ def autoroi(img):
 
     thresh = cv2.threshold(gray_img, 130, 255, cv2.THRESH_BINARY)[1]
     thresh = cv2.dilate(thresh, None, iterations=5)
-S
+
     contours, hierarchy = cv2.findContours(
         thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     biggest = max(contours, key=cv2.contourArea)
     x, y, w, h = cv2.boundingRect(biggest)
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 224, 0), 2)
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 256, 0), 2)
     roi = img[y:y+h, x:x+w]
 
     return roi
 
 
 def prediction():
-    list_of_files = glob.glob('data/validation/not_glaucoma/*') #testing different files
+    list_of_files = glob.glob('data/validation/glaucoma/ROI - 01_h.jpg.png') #testing different files
     latest_file = max(list_of_files, key=os.path.getctime)
     img = cv2.imread(latest_file)
     img = autoroi(img)
-    img = cv2.resize(img, (224, 224))
-    img = np.reshape(img, [1, 224, 224, 3])
+    img = cv2.resize(img, (256, 256))
+    img = np.reshape(img, [1, 256, 256, 3])
     img = tf.cast(img, tf.float64)
 
 
     prediction = model.predict(img)
     print(prediction)
-    # prediction = prediction.argmax(axis=1)
+    prediction = prediction.argmax(axis=1)
     print(prediction)
 
 
@@ -60,7 +60,7 @@ def prediction():
 
 finalPrediction = prediction()
 print "Final Prediction = ", finalPrediction
-if (finalPrediction == 1):
+if (finalPrediction < 0.5):
     print("Congratulations! You are healthy!")
 else:
     print("Unfortunately, you have been diagnosed with glaucoma.")
